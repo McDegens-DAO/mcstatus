8af470987dccc6fc32ce25e88e3a0963b5b4a1f8
McDegens <137731698+McDegens@users.noreply.github.com>
2024-06-09 18:34:12 -0400

    Optimizations

--- txFinalized.js
+++ txFinalized.js
@@ -1,42 +1,40 @@
-// verifies finalized status of signature
-async function txFinalized(cluster, sig, max = 40, seconds = 4){
+async function txFinalized(sig,max=10,int=4){
   return await new Promise(resolve => {
     let start = 1;
-    seconds = (seconds * 1000);
-    let connection = new solanaWeb3.Connection(cluster, "confirmed");
+    let connection = new solanaWeb3.Connection(conf.cluster, "confirmed");
     let intervalID = setInterval(async()=>{
-      console.log(start+": "+sig);
-      let tx_status = await connection.getSignatureStatuses([sig], {searchTransactionHistory: true,});
-      console.log(tx_status.value[0]);
-      if(start > 20 && tx_status.value[0] == null){
-        clearInterval(intervalID);
-        console.log('Oh No! Something Happened!');
-        resolve('Oh No! Something Happened!');
+      let tx_status = null;
+      tx_status = await connection.getSignatureStatuses([sig], {searchTransactionHistory: true,});
+      console.log(start);
+      console.log(sig);
+      if (tx_status != null && typeof tx_status.value != "undefined"){ 
+        console.log(tx_status.value);
       }
-      if (typeof tx_status == "undefined" || 
+      else{
+        console.log("failed to get status...");
+      }
+      if (tx_status == null || 
       typeof tx_status.value == "undefined" || 
       tx_status.value == null || 
       tx_status.value[0] == null || 
       typeof tx_status.value[0] == "undefined" || 
       typeof tx_status.value[0].confirmationStatus == "undefined"){} 
-      else if (tx_status.value[0].confirmationStatus == "processed"){
-        console.log('Transaction Processed!');
-        start = 1;
-      }
-      else if(tx_status.value[0].confirmationStatus == "confirmed"){
-        console.log('Transaction Confirmed!');
+      else if(tx_status.value[0].confirmationStatus == "processed"){
         start = 1;
       }
       else if (tx_status.value[0].confirmationStatus == "finalized"){
-        console.log('Transaction Complete!');
-        clearInterval(intervalID);
+        if(tx_status.value[0].err != null){
+          resolve('program error!');
+          clearInterval(intervalID);
+        }
         resolve('finalized');
+        clearInterval(intervalID);
       }
       start++;
-      if(start==max){
+      if(start == max + 1){
+        resolve((max * int)+' seconds max wait reached');
         clearInterval(intervalID);
-        resolve(max+' max retrys reached');
       }
-    }, seconds);
+    },(int * 1000));
   });  
 }
